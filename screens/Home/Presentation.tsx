74c5b2bcdaa3d902e28e2891a58ed4bd9ad4666f
Matias <matias.sanchez.0097@gmail.com>
2022-08-30 16:40:10 -0300

    fix problem image

--- screens/Home/Presentation.tsx
+++ screens/Home/Presentation.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Image from "next/image";
 
-import { Box, Stack, Text, useMediaQuery } from "@chakra-ui/react";
+import { Box, Image, Stack, Text, useMediaQuery } from "@chakra-ui/react";
 
 import Section from "../../components/section/Section";
 
